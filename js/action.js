d4e1fc67498cc2c4eab0d85d5bb136e645fbc638
Unknown <carloshallan@gmail.com>
2018-10-06 11:37:05 -0300

    Arquivo de include removido
    Problemas com a execução de funções autoexecutáveis. Mesmo o arquivo do include carregando primeiro, o arquivo com a função de click era mais rápido e acabava não adicionando o evento,  para resolver chamei a ação só após o carregamento do completo do include.

--- js/action.js
+++ js/action.js
@@ -1,5 +1,33 @@
 $(document).ready(function () {
     // get height header
+
+    (function includeHTML() {
+        let include = $('[include]');
+    
+        $.each(include, function (index, includeElem) {
+    
+            let includeMenu = new XMLHttpRequest();
+            let includeE = $(includeElem);
+            let includeURL = $(includeElem).attr('include');
+    
+            includeMenu.open('GET', includeURL);
+    
+            includeMenu.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {includeE.html(this.responseText);}
+                    if (this.status == 404) {includeE.html("Page not found.");}
+                    /*remove the attribute, and call this function once more:*/
+                    actions();
+                    includeE.removeAttr("include");
+                    
+                  }
+            }
+    
+            includeMenu.send();
+        });
+    }());
+
+
     (function styles() {
 
         let windowHeight = window.innerHeight;
@@ -8,8 +36,22 @@ $(document).ready(function () {
             'margin-top': windowHeight
         });
 
+    }());
 
-        $('.iconMenu').on('click', function () {
+    function showSearch() {
+
+        let hasSearchShow = $('.search input').hasClass('searchShow');
+
+        if (hasSearchShow) {
+            $('.search input').removeClass('searchShow');
+        } else {
+            $('.search input').addClass('searchShow');
+        }
+
+    }
+
+    function actions(){
+        $('.iconMenu').on('click', function () { // add click event open menu
             let classBar = $(this).find('div').hasClass('bar');
             let classToogleBar = $(this).find('div').hasClass('toggleBar');
 
@@ -35,19 +77,6 @@ $(document).ready(function () {
         $('.iconSearch').on('click', function () {
             showSearch();
         });
-
-    }());
-
-    function showSearch() {
-
-        let hasSearchShow = $('.search input').hasClass('searchShow');
-
-        if (hasSearchShow) {
-            $('.search input').removeClass('searchShow');
-        } else {
-            $('.search input').addClass('searchShow');
-        }
-
     }
 
 });
